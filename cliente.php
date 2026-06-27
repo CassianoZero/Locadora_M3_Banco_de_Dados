6c48b8a503bb6ac09eef5aed92057949dff64d42
CassianoOnorio <cassianoonorio@gmail.com>
2026-06-27 14:45:26 -0300

    Update cliente.php

--- cliente.php
+++ cliente.php
@@ -151,7 +151,19 @@ $resultado = $conexao->query("
     <title>Clientes</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
+
+<header class="topo">
+    <div class="logo">Locadora M3</div>
+
+    <nav class="menu">
+        <a href="cliente.php">Clientes</a>
+        <a href="veiculo.php">Veículos</a>
+        <a href="reserva.php">Reservas</a>
+        <a href="locacao.php">Locações</a>
+    </nav>
+</header>
 
 <h1>Cadastro de Clientes</h1>
 
@@ -207,6 +219,10 @@ $resultado = $conexao->query("
     </tr>
     <?php } ?>
 </table>
+
+<footer>
+    <p>Trabalho M3 - Banco de Dados - Tema: Aluguel de Carros</p>
+</footer>
 
 </body>
 </html>
